abf27c86e9345dac8cbaa3ed28474ad87e410594
Jacek Grzegorczyk <jacek.grzegorczyk@britenet.com.pl>
2022-11-30 15:11:58 +0100

    Add: log received message in Notifier

--- notifier/src/main/java/dev/jacek/grzegorczyk/messages/KafkaListeners.java
+++ notifier/src/main/java/dev/jacek/grzegorczyk/messages/KafkaListeners.java
@@ -1,14 +1,16 @@
 package dev.jacek.grzegorczyk.messages;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class KafkaListeners {
 
     @KafkaListener(topics = "registered-messages",
             groupId = "notifier_group_id")
     void listener(String data) {
-        System.out.println("LISTENER RECEIVED: " + data);
+        log.info("LISTENER RECEIVED: {}", data);
     }
 }
