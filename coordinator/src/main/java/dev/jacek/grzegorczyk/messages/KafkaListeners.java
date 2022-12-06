45637ab70492eece8aa66620440f2d608c46c8aa
Jacek Grzegorczyk <jacek.grzegorczyk@britenet.com.pl>
2022-12-06 12:28:59 +0100

    Add: KafkaMessage consumer converter

--- coordinator/src/main/java/dev/jacek/grzegorczyk/messages/KafkaListeners.java
+++ coordinator/src/main/java/dev/jacek/grzegorczyk/messages/KafkaListeners.java
@@ -1,7 +1,6 @@
 package dev.jacek.grzegorczyk.messages;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.jacek.grzegorczyk.entities.ApiMessage;
 import dev.jacek.grzegorczyk.service.ApiMessageService;
@@ -21,26 +20,22 @@ public class KafkaListeners {
     private final ApiMessageService apiMessageService;
 
     @KafkaListener(topics = "outbox.event.Registration",
-            groupId = "coordinator_group_id")
-    void listener(String data) {
+            groupId = "coordinator_group_id",
+            containerFactory = "kafkaMessageListenerContainerFactory")
+    void listener(KafkaMessage kafkaMessage) {
 
+        System.out.println("LISTENER RECEIVED: " + kafkaMessage);
+        Payload payload = kafkaMessage.getPayload();
+        String registrationPayloadMessage = payload.getPayload();
         try {
-            System.out.println("LISTENER RECEIVED: " + data);
-            KafkaMessage kafkaMessage = objectMapper.readValue(data, new TypeReference<>() {
-            });
-            Payload payload = kafkaMessage.getPayload();
-            String registrationPayloadMessage = payload.getPayload();
-            RegistrationPayload registrationPayload = objectMapper.readValue(registrationPayloadMessage, new TypeReference<>() {
-            });
+            RegistrationPayload registrationPayload = objectMapper.readValue(registrationPayloadMessage, RegistrationPayload.class);
             log.info("KAFKA MESSAGE DESERIALIZED: {}", registrationPayload);
 
             String contents = registrationPayload.getContents();
-            ApiMessage apiMessage = objectMapper.readValue(contents, new TypeReference<>() {
-            });
+            ApiMessage apiMessage = objectMapper.readValue(contents, ApiMessage.class);
             apiMessageService.setIsRegistered(apiMessage.getId());
 
             kafkaTemplate.send("registered-messages", "1", contents);
-
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
