132ad74edd243ad55e9311635ab66062b1c35453
Jacek Grzegorczyk <jacek.grzegorczyk@britenet.com.pl>
2022-12-20 11:33:30 +0100

    Add: sleuth log trace id

--- coordinator/src/main/java/dev/jacek/grzegorczyk/web/ApiMessageController.java
+++ coordinator/src/main/java/dev/jacek/grzegorczyk/web/ApiMessageController.java
@@ -5,6 +5,8 @@ import dev.jacek.grzegorczyk.model.ApiMessageDTOOut;
 import dev.jacek.grzegorczyk.service.ApiMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,10 +21,17 @@ public class ApiMessageController {
 
     private final ApiMessageService apiMessageService;
 
+    private final Tracer tracer;
+
     @PostMapping
     @ResponseStatus(CREATED)
     public ApiMessageDTOOut create(@RequestBody @Valid ApiMessageDTO apiMessage) {
         log.info("API received a new message: {}", apiMessage);
+        Span span = tracer.currentSpan();
+        if (span != null) {
+            log.info("Trace ID {}", span.context().traceId());
+            log.info("Span ID {}", span.context().spanId());
+        }
         return apiMessageService.create(apiMessage);
     }
 }
