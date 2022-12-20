457ae832c76b4bb9097f05b88ba7f77546d8e9c3
Jacek Grzegorczyk <jacek.grzegorczyk@britenet.com.pl>
2022-12-20 14:30:44 +0100

    Add: service - http-handler-1

---
+++ http-handler-1/src/main/java/dev/jacek/grzegorczyk/HttpHandler1Application.java
@@ -0,0 +1,11 @@
+package dev.jacek.grzegorczyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpHandler1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HttpHandler1Application.class, args);
+    }
+}
---
+++ http-handler-1/src/main/java/dev/jacek/grzegorczyk/web/ApiMessageController.java
@@ -0,0 +1,32 @@
+package dev.jacek.grzegorczyk.web;
+
+import dev.jacek.grzegorczyk.models.ApiMessageDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/messages")
+@RequiredArgsConstructor
+public class ApiMessageController {
+
+    private final Tracer tracer;
+
+    @PostMapping
+    public void create(@RequestBody @Valid ApiMessageDTO apiMessage) {
+        log.info("API received a new message: {}", apiMessage);
+        Span span = tracer.currentSpan();
+        if (span != null) {
+            log.info("Trace ID {}", span.context().traceId());
+            log.info("Span ID {}", span.context().spanId());
+        }
+    }
+}
